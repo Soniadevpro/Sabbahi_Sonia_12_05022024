1ff3f801fcf0a5d17e1b710e1c35a654531a30d1
Sonia Sabbahi <soniadevpro@gmail.com>
2024-03-12 23:54:15 +0100

    navbar scroll

--- portfolio-sonia/src/components/header/header.tsx
+++ portfolio-sonia/src/components/header/header.tsx
@@ -15,22 +15,42 @@ const navigationItems: NavigationItem[] = [
 ];
 
 const Header: React.FC = () => {
+  const [activeSection, setActiveSection] = useState<string | null>(null);
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
+      // Mettez à jour `scrolled` basé sur la position de défilement.
       const isScrolled = window.scrollY > 100;
       if (isScrolled !== scrolled) {
         setScrolled(isScrolled);
       }
+  
+      // Exemple simplifié de logique pour déterminer la section active.
+      // Cela suppose que chaque section a un offsetTop et une hauteur qui peut être comparée à window.scrollY.
+      let currentSectionId = null;
+      for (const item of navigationItems) {
+        const sectionElement = document.getElementById(item.id);
+        if (sectionElement) {
+          const sectionTop = sectionElement.offsetTop;
+          const sectionHeight = sectionElement.offsetHeight;
+          if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+            currentSectionId = item.id;
+            break; // Sortez de la boucle une fois la section actuelle trouvée.
+          }
+        }
+      }
+      setActiveSection(currentSectionId); // Mettez à jour l'état avec l'ID de la section actuelle.
     };
-
+  
+    // Écoutez l'événement de défilement pour mettre à jour la section active.
     window.addEventListener('scroll', handleScroll);
-
+    handleScroll(); // Appel initial pour définir la section active lors du chargement de la page.
+  
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [scrolled]);
+  }, [scrolled]); // Dépendances de l'effet
 
   const scrollToSection = (id: string): void => {
     const section = document.getElementById(id);
@@ -42,10 +62,12 @@ const Header: React.FC = () => {
   return (
     <div>
       <header>
+        <div className="containernavbar">
+
       <div className={`navbar ${scrolled ? 'scrolled' : ''}`} >
           <ul>
             {navigationItems.map((item) => (
-              <li key={item.id} >
+              <li key={item.id} className={activeSection === item.id ? 'scrolled' : ''}>
                 {item.action ? (
                   <button onClick={() => scrollToSection(item.id)}>{item.name}</button>
                 ) : (
@@ -54,6 +76,7 @@ const Header: React.FC = () => {
               </li>
             ))}
           </ul>
+        </div>
         </div>
       </header>
     </div>
