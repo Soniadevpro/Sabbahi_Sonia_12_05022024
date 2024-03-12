96fb59e3319316b12b8cd7a91da8c99ef79a5fe1
Sonia Sabbahi <soniadevpro@gmail.com>
2024-03-12 23:22:41 +0100

    style navbar

--- portfolio-sonia/src/components/header/header.tsx
+++ portfolio-sonia/src/components/header/header.tsx
@@ -1,27 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import "./header.scss"
+interface NavigationItem {
+  name: string;
+  id: string;
+  action?: string;
+}
 
+const navigationItems: NavigationItem[] = [
+  { name: '0.Home', id: 'home-section' },
+  { name: '1.Skills', id: 'skills-section', action: 'scrollToSkills' },
+  { name: '2.Projects', id: 'projects-section' },
+  { name: '3.Courses', id: 'courses-section' },
+  { name: '4.Contact', id: 'contact-section' },
+];
 
-const header = () => {
-  const scrollToSkills = () => {
-    const skillsSection = document.getElementById('skills-section');
-    if (skillsSection) {
-        skillsSection.scrollIntoView({ behavior: 'smooth' });
+const Header: React.FC = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 100;
+      if (isScrolled !== scrolled) {
+        setScrolled(isScrolled);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [scrolled]);
+
+  const scrollToSection = (id: string): void => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
     }
-};
-    return (
-        <div>
-             <header>
-      <div className ="navbar">
-        <ul>
-          <li>0.Home</li>
-          <li><button onClick={scrollToSkills}>1.Skills</button></li>
-          <li>2.Projects</li>
-          <li>3.Courses</li>
-          <li>4.Contact</li>
-        </ul>
-      </div>
-    </header>
+  };
+
+  return (
+    <div>
+      <header>
+      <div className={`navbar ${scrolled ? 'scrolled' : ''}`} >
+          <ul>
+            {navigationItems.map((item) => (
+              <li key={item.id} >
+                {item.action ? (
+                  <button onClick={() => scrollToSection(item.id)}>{item.name}</button>
+                ) : (
+                  <span>{item.name}</span> // or use <a href={`#${item.id}`}> for actual links
+                )}
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+      </header>
+    </div>
+  );
 };
 
-export default header;
+export default Header;
+
+
+
+// style={{ backgroundColor: scrolled ? 'white' : 'transparent' }}
