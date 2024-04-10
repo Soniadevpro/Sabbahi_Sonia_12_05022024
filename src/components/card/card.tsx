64f32c594621b9f9c62fd279ce05d9899ddba3bf
Sonia Sabbahi <soniadevpro@gmail.com>
2024-04-10 22:57:22 +0200

    Update courses

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -3,7 +3,7 @@
 import kasaPic from '../../assets/DALL·E 2024-04-10 22.24.44 - Imagine a sleek and engaging web interface for a peer-to-peer lodging platform, with a coral color scheme dominating the design. The homepage is desig.webp';
 import sophiaPic from '../../assets/DALL·E 2024-04-10 22.26.49 - Envision a minimalist and elegant website design for an architect, featuring a color palette of green and beige. The homepage is structured to immedia.webp';
 import argetnPics from '../../assets/DALL·E 2024-04-10 22.20.33 - Imagine a sleek and modern user interface for a banking application login screen. The primary color scheme is shades of green, symbolizing security an.webp';
-
+import oc from "../../assets/logo-OpenClassRooms-COM-du-SITE-WEB-9.jpg"
 
 
 function Card() {
@@ -45,6 +45,31 @@ function Card() {
         <button>Redux</button>
       </figcaption>
     </figure>
+
+     </div>
+     <h3 >Formations</h3>
+     <div className="position-block">
+    <figure className="image-block-courses">
+      <h1>Sophia Bluel Architecte</h1>
+      <img src={sophiaPic} alt="" />
+      <figcaption>
+        <h2>Plus d'informations</h2>
+        <p>Expert React - Typescript </p>
+        <button>Github</button>
+        <button>Javascript</button>
+      </figcaption>
+    </figure>
+      <figure className="image-block-courses">
+      <h1>KASA</h1>
+      <img src={oc} alt="" className='oc'/>
+      <figcaption>
+      <h2>Plus d'informations</h2>
+        <p>Titre RNCP de niveau 5 - Bac+2.</p>
+        <button>Informations sur openclassrooms</button>
+       
+      </figcaption>
+    </figure>
+ 
 
      </div>
     </div>
