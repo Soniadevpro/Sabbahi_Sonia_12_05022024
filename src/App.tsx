47e04e56540aefe18869e0582dd55e040dc387bf
Sonia Sabbahi <soniadevpro@gmail.com>
2024-04-15 19:32:38 +0200

    passage de card dans les views

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // Assurez-vous d'importer vos composants. Décommentez les lignes suivantes et assurez-vous que les chemins d'importation sont corrects.
 import Home from "./views/home/home";
 import Skills from "./views/skills/skills";
-import Projects from "./views/projects/projects";
 import Contact from "./views/contact/contact";
+import Footer from "./components/footer/footer"
+import Header from "./components/header/header";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <Header />
       <Routes>
         {/* Assurez-vous d'avoir les composants importés pour les utiliser ici */}
         <Route path="/" element={<Home />} />
         
         <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
+        {/* <Route path="/projects" element={<Projects />} /> */}
         <Route path="/contact" element={<Contact/>} />
         {/* <Route path="*" element={<Erreur />} /> */}
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </BrowserRouter>
   );
 };
--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,21 +1,20 @@
-import Header from "../../components/header/header";
+
 import Hero from "../../components/hero/hero";
 import Badge from "../../components/badge/badge"
 import Skills from "../skills/skills";
-import Projects from "../../views/projects/projects"
-
 import Contact from "../contact/contact";
-import Footer from "../../components/footer/footer"
+import Card from "../card/card";
+
 const home = () => {
     return (
         <div>
-            <Header />
+        
             <Hero/> 
             <Badge/>
             <Skills/>
-            <Projects/>
+            <Card/>
             <Contact/>
-            <Footer/>
+  
         </div>
     );
 };
